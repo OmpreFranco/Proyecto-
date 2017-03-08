6b14d960dd6c2290ab9e450c919cec22f0b59b61
franco <franco@WTPC>
2017-03-08 18:04:27 -0300

    Cambios en el clase_animal.py

--- clase_Animal.py
+++ clase_Animal.py
@@ -14,12 +14,70 @@ import math as mat
 class Animal():
     
     def __init__(self,radio,  vel,  life,  agresividad):
-		self.position.x = 0
-		self.position.y = 0
+		self.position_x = 0
+		self.position_y = 0
 		self.perceptionRadio = radio
 		self.velocity = vel
 		self.life = life
 		self.objetivo = 0
-		self.type = TypeOfAnimal::getTypeOfAnimal(agresividad)
-  
-  
+		self.type = TypeOfAnimal(agresividad)
+ 
+
+    def setPosition(pos):
+        self.position = pos
+        
+    def getPosition(self):
+        return position 
+        
+    def
+#void Animal::setPosition(Position pos){
+#	this->position = pos;
+#}
+#Position Animal::getPosition(){
+#		return position;
+#}
+#
+#void Animal::scout(Animal *animalsArray[], int cant){
+#	//En cada busqueda puedo o no detectar un objetivo
+#		int minDistance = 9999;
+#		Animal *obj =NULL;
+#		//Controlo TODOS los animales del ambiente para determinar cual es mi objetivo mas cercano
+#		for(int i=0;i<cant;i++){
+#			if(type->esObjetivo(animalsArray[i]) && minDistance > this->distance(animalsArray[i])){
+#				minDistance = this->distance(animalsArray[i]);
+#				obj = animalsArray[i];
+#			}
+#		}
+#		this->setObjetivo(obj); //Guardo cual es mi objetivo, para poder moverme de acuerdo al mismo
+#	}
+#
+#void Animal::setObjetivo(Animal *animal){
+#		this->objetivo= animal;
+#	}
+#
+#TypeOfAnimal *Animal::getType(){
+#	//Esta funcion retorna el tipo al cual pertenece el animal para poder actuar en base a su tipo
+#	return type;
+#}
+#
+#void Animal::move(){
+#	if(this->objetivo) {//Si tengo un objetivo -  (objetivo != NULL)
+#		//Tengo un objetivo que perseguir, me muevo de acuerdo al objetivo
+#		this->getType()->actuar(this, this->objetivo);
+#	}
+#	else{ //No detecte objetivo en mi busqueda
+#		//Determino un movimiento aleatorio para el animal
+#	}
+#}
+#
+#bool Animal::estaVivo(){
+#	return life > 0;
+#}
+#
+#float Animal::distance(Animal* a){
+#	Position P1 = this->getPosition();
+#	Position P2 = a->getPosition();
+#	float xVal = P2.x - P1.x;
+#	float yVal = P2.y - P1.y;
+#	return sqrt(xVal*xVal + yVal*yVal);
+#}  
