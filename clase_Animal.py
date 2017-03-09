a3bd4539318fd4026510d15bf5d7d7183ef4dcb7
franco <franco@WTPC>
2017-03-09 18:57:49 -0300

    ultimos cambios. Listo para documentacion

--- clase_Animal.py
+++ clase_Animal.py
@@ -45,7 +45,7 @@ class Animal():
                 #defino el versor donde apunta la direccion que une ambos objetos
                 r_versor = [delta_x,delta_y ] / distancia
                 self.position = self.position + r_versor * self.velocity 
-                print "alla voy"
+                print "alla voy,preparate gil"
             
             else:    
                 self.position = objetivo.position 
@@ -56,6 +56,7 @@ class Animal():
         #Creo un modo de que el animal camine aleatoriamente
         else:
             print "nada por aqui"
+            
             a = np.random.random(1)*2*np.pi # Genero el angulo aleatorio 
             self.position[0] = self.position[0] + np.cos(a)*self.velocity # Marco el cambio de posicion en X e Y 
             self.position[1] = self.position[1] + np.sin(a)*self.velocity
@@ -63,11 +64,13 @@ class Animal():
                 
     #  Metodo para hacer que el animal sense su entorno    
     def scout(self,agentes): # Agentes seria la lista de animales en el ambiente
-        objetivo = None    
+        objetivo = None 
+        minDistance = 9999
         for i in agentes:
             pos_agent = i.return_Position()
             distancia = np.sqrt((pos_agent[0]-self.position[0])**2 + (pos_agent[1]-self.position[1])**2)
-            if distancia < self.perceptionRadio and distancia != 0:
+            if distancia <= self.perceptionRadio and distancia != 0 and minDistance > distancia:
+                minDistance = distancia
                 objetivo = i 
                 print "detecte algo"        #Tenemos que ver que hace cuando detecto algo 
             # Aca mi animal no detecta nada  
