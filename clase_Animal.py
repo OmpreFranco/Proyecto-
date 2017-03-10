4762079e197aed2b5fd82063507edaed1fed8b71
CrisWere <cristina.werenitzky@gmail.com>
2017-03-10 12:18:12 -0300

    Clase Animal documentada

--- clase_Animal.py
+++ clase_Animal.py
@@ -1,29 +1,47 @@
-
 import numpy as np 
 #from matplotlib import  pyplot as plt
 #import math as mat
 import clase_type
 
-#==============================================================================
-# definimos la clase animal
-#==============================================================================
 class Animal():
     # Atributos de la clase animal 
+    """	Descripcion de la clase Animal
+
+    La clase **Animal** permite crear los animales que estaran en la simulacion
+
+     - **Parametro de construccion**:
+		:position, ``type <array>``: posicion asignada al animal dentro del ambiente
+	        :perceptionRadio, ``type <float>``: radio de percepcion del animal
+		:velocity, ``type <float>``: velocidad del animal en un paso de tiempo?????
+	        :life, ``type <bool>``: Valor que determina si el animal esta vivo
+		:type, ``type <:class:`TypeOfAnimal`>``: tipo que posee el animal (:class:`Hunter` o :class:`Victim`)
+
     """
-    La clase **Animal** crea los animales que estaran en la simulacion.
-    
-    """
-    def __init__(self,radio_vision, position , vel,  life,  agresividad): #position es un areglo de dos elementos [X,Y], de que objeto la obtengo ?
+
+    def __init__(self,radio_vision, position , vel,  life,  agresividad): 
         self.position = position
         self.perceptionRadio = radio_vision
         self.velocity = vel
         self.life = life
-        self.agresividad = agresividad
+        #self.agresividad = agresividad    #Creo que esto ya no iria
         tipo = clase_type.TypeOfAnimal(agresividad)
         self.type = tipo.getType(agresividad)  
+
+
     # Metodo de clase para establecer la posicion 
     def return_Position(self):
-#        self.position = position
+	# self.position = position
+        """ Metodo que retorna la posicion de un animal
+
+        Este metodo retorna la posicion asignada al animal dentro del :class:`Ambiente` 
+
+        **Parameters** 
+		
+`
+        **Returns** 
+		:position, type <array>
+
+        """
         return self.position
    
    
@@ -31,7 +49,15 @@ class Animal():
     
     def move(self,objetivo):
        
-       
+        """ Metodo que indica calcula el movimiento del animal
+
+        Este metodo determina el movimiento del animal de acuerdo al tipo que posee y a si 
+        detecto un objetivo
+
+        **Parameters** 
+		:objective, type <:class:`Animal`>: el animal se mueve en relacion al objetivo que posee
+
+        """       
        #Creo un modo de que el animal vaya a la posicion de su objetivo si su agresividad es mayor        
         if objetivo != None and self.agresividad != objetivo.agresividad:
 
@@ -64,6 +90,18 @@ class Animal():
                 
     #  Metodo para hacer que el animal sense su entorno    
     def scout(self,agentes): # Agentes seria la lista de animales en el ambiente
+        """ Metodo que realiza la busqueda de un nuevo objetivo
+
+        Este metodo retorna el objetivo encontrado en el :class:`Ambiente` 
+
+        **Parameters** 
+		:agents, type <array>		
+`
+        **Returns** 
+		:position, type <array>
+
+        """
+
         objetivo = None 
         minDistance = 9999
         for i in agentes:
